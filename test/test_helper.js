da21176e2e96189b52027c9c28dca3782485ec42
Michael Christenson II <michael@rebelhold.com>
2014-03-04 22:02:04 -0700

    added pending acceptance tests for setup

--- test/test_helper.js
+++ test/test_helper.js
@@ -1,2 +1,7 @@
+process.env.NODE_ENV = 'test';
+
 chai   = require('chai');
-assert = require('assert');
+assert = chai.assert;
+should = chai.should;
+expect = chai.expect;
+chai.use(require('chai-fs'));
---
+++ test/unit/setup.js
@@ -0,0 +1,42 @@
+require('../test_helper');
+
+var path        = require('path'),
+    fs          = require('fs');
+
+describe('setup', function () {
+  it('returns a temptress object without params setup');
+  it('returns a temptress object with params setup');
+  it('has a config');
+  describe('config.env', function () {
+    it('defaults to development');
+    it('sets to current environment');
+  });
+  describe('config.paths', function () {
+    it('sets a destination path');
+    it('sets a library path');
+    it('sets a preview path');
+  });
+  describe('paths.create', function () {
+    it('creates a destination path');
+    it('creates a library path');
+    it('creates a preview path');
+  });
+  describe('library.clone', function () {
+    it('copies library templates to destination path');
+  });
+  describe('library.templates', function () {
+    it('has templates with template objects');
+    describe('template', function () {
+      it('has a path');
+      it('has a name');
+      it('has a preview path');
+      describe('template.preview', function () {
+        it('returns a preview object');
+        describe('preview', function () {
+          it('creates a preview path');
+          it('copies template into preview path');
+        });
+      });
+    });
+  });
+});
